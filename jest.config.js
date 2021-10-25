701818a8e5ae86536671f8e3715af79d7862e594
Felipe Lara <68015575+larafe1@users.noreply.github.com>
2021-10-24 23:25:02 -0300

    Update: Jest config & Tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['src/hooks/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
